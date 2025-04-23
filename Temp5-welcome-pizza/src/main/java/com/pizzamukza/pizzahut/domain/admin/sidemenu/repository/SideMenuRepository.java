78f11dd6dc8721ec1acafe4d73e275a0d2d40b3c
FerryLa <rlawl4240@gmail.com>
2025-04-24 00:56:44 +0900

    [사이드 메뉴 관리] 기능 구현 초안입니다.

--- Temp5-welcome-pizza/src/main/java/com/pizzamukza/pizzahut/domain/admin/sidemenu/repository/SideMenuRepository.java
+++ Temp5-welcome-pizza/src/main/java/com/pizzamukza/pizzahut/domain/admin/sidemenu/repository/SideMenuRepository.java
@@ -4,14 +4,17 @@ import com.pizzamukza.pizzahut.domain.admin.sidemenu.dto.SideMenu;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+import static com.pizzamukza.common.JDBCTemplate.close;
+import static com.pizzamukza.common.JDBCTemplate.getConnection;
+
 public class SideMenuRepository {
 
-    private Properties props = null;
+    private static Properties props = null;
 
     public SideMenuRepository() {
         props = new Properties();
@@ -22,14 +25,61 @@ public class SideMenuRepository {
         }
     }
 
-    public List<SideMenu> selectSideMenu() {
+    public static List<SideMenu> selectAllSideMenu(Connection con) {
 
         PreparedStatement pstmt = null;
-        ResultSet rs = null;
+        ResultSet rset = null;
+        String sql = props.getProperty("selectAllSideMenu");
 
-        System.out.println();
+        System.out.println("sql : " + sql);
 
-        String sql = props.getProperty("selectSideMenu");
+        List<SideMenu> sideMenus = null;
+        try {
+            rset = pstmt.executeQuery();
+            sideMenus = new ArrayList<>();
+            while (rset.next()) {
+                SideMenu sm = new SideMenu();
+                sm.setSideId(rset.getInt("sideId"));
+                sm.setSideName(rset.getString("sideName"));
+                sm.setPrice(rset.getInt("setPrice"));
+                sm.setQuantity(rset.getInt("quantity"));
 
+                sideMenus.add(sm);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            close(rset);
+            close(pstmt);
+        }
+        return sideMenus;
     }
+
+    /* 잘 안되는 쿼리문 호출 (목록조회) */
+//    public static List<SideMenu> selectAllSideMenu2(Connection con) {
+//        Statement stmt = null;
+//        ResultSet rset = null;
+//
+//        String query = "SELECT * FROM tbl_side_menu";
+//        System.out.println("query = " + query);
+//
+//        try {
+//            stmt = con.createStatement();
+//            rset = stmt.executeQuery(query);
+//
+//            if (rset.next()) {
+//                System.out.println(rset.getString("sideName") + "");
+//            } else {
+//                System.out.println("메뉴 정보가 없습니다.");
+//            }
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        } finally {
+//            close(rset);
+//            close(stmt);
+//            close(con);
+//
+//        }
+//    }
 }
--- Temp5-welcome-pizza/src/main/java/com/pizzamukza/pizzahut/domain/admin/sidemenu/view/SideMenuView.java
+++ Temp5-welcome-pizza/src/main/java/com/pizzamukza/pizzahut/domain/admin/sidemenu/view/SideMenuView.java
@@ -1,5 +1,9 @@
 package com.pizzamukza.pizzahut.domain.admin.sidemenu.view;
 
+import com.pizzamukza.common.CommonMenuView;
+import com.pizzamukza.pizzahut.domain.admin.sidemenu.dto.SideMenu;
+import com.pizzamukza.pizzahut.domain.admin.sidemenu.service.SideMenuService;
+
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -8,74 +12,58 @@ public class SideMenuView {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        SideMenuService sm = new SideMenuService();
+
+//        sm.selectAllSideMenus();
+
+        /* 관리자 메뉴 출력 (관리자 로그인시 공동) */
+        CommonMenuView.printAdminServiceMenu();
+        int input = sc.nextInt();
+        switch (input) {
+            case 1 : break;
+            case 2 : printSideMenuStart();
+                break;
+            case 0 : CommonMenuView.printAdminMenu();
+                break;
+            default :
+                System.out.println("번호를 잘못 입력하셨습니다.");
+        }
+
+    }
+
+    /* 초기 메인화면으로 돌아가기 */
+    public static void printSideMenuBack() {
+        CommonMenuView.printMainMenu();
+    }
+
+     /* 사이드 관리 화면 출력 */
+    public static void printSideMenuStart() {
+        Scanner sc = new Scanner(System.in);
 
         String start = """
-                --- 사이드 메뉴 관리 ---
+                ====== 사이드 관리 ======
                 1. 사이드 목록 조회
                 2. 사이드 등록
                 3. 사이드 삭제
                 4. 사이드 가격 수정
                 0. 이전 메뉴로 돌아가기
-                ======================
-                번호를 입력하세요: """;
-
-//        switch (true) {
-//            System.out.print(start);
-//            int startNum = sc.nextInt();
-//            case 1:
-//                break;
-//            case 2: break;
-//            case 3: break;
-//            case 4: break;
-//            case 5: break;
-//        }
-
-        System.out.println("번호를 잘못 입력하셨습니다.");
-
-        /*
-        * list
-                --------------- 사이드 메뉴 목록 ---------------
-                1. 감자튀김 | 가격: 3,000원 | 수량: 20
-                2. 콜라     | 가격: 2,500원 | 수량: 15
-                3. 치즈스틱 | 가격: 4,500원 | 수량: 10
-                =============================================
-                0. 이전 메뉴로 돌아가기
-                * */
-
-
-
-        /*
-        insert
-        --- 사이드 등록 ---
-        사이드 메뉴 이름을 입력하세요: 감자튀김
-        가격을 입력하세요: 3000
-        수량을 입력하세요: 20
-
-        ✅ 사이드 메뉴가 등록되었습니다.
-        """;
-         */
-
-        /*
-        * delete
-        * """
-                --- 사이드 삭제 ---
-                삭제할 사이드 번호를 선택하세요: 1
-
-                ✅ 감자튀김이 삭제되었습니다.
+                =======================
+                번호를 입력하세요:  
                 """;
-        * */
-        /* update
-        * """
-                --- 사이드 가격 수정 ---
-                수정할 사이드 번호를 선택하세요: 2
-                새 가격을 입력하세요: 2500
 
-                ✅ 사이드 메뉴 가격이 수정되었습니다.
-
-                """;
-        * */
-
-
-
+        System.out.print(start);
+        int input = sc.nextInt();
+        switch (input) {
+            case 1 : CommonMenuView.printAdminServiceMenu();
+                break;
+            case 2 : break;
+            case 3 : break;
+            case 4 : break;
+            case 5 : break;
+            case 0 :
+                break;
+            default :
+                System.out.println("번호를 잘못 입력하셨습니다.");
+        }
     }
 }
