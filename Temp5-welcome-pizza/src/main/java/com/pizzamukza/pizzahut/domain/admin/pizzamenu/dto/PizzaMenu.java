729ee951ee6a1c9ff9498fbd613a215e62ad15b4
hyeblee <hyeb1224@naver.com>
2025-04-24 02:54:38 +0900

    feat: PizzaMenu dto 필드 정의

--- Temp5-welcome-pizza/src/main/java/com/pizzamukza/pizzahut/domain/admin/pizzamenu/dto/PizzaMenu.java
+++ Temp5-welcome-pizza/src/main/java/com/pizzamukza/pizzahut/domain/admin/pizzamenu/dto/PizzaMenu.java
@@ -1,4 +1,8 @@
 package com.pizzamukza.pizzahut.domain.admin.pizzamenu.dto;
 
 public class PizzaMenu {
+  int pizzaId;
+  String pizzaName; // 페퍼로니피자 미디움, 페퍼로니피자 라지
+  int pizzaPrice;
+  int quantity; // 가격
 }
