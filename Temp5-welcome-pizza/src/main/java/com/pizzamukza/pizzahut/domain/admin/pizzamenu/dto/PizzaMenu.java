0492467ce1e21d7301f96ba5fd28fe296c7eda72
hyeblee <hyeb1224@naver.com>
2025-04-24 05:46:45 +0900

    feat: 피자 주문 - 피자 선택 구현

--- Temp5-welcome-pizza/src/main/java/com/pizzamukza/pizzahut/domain/admin/pizzamenu/dto/PizzaMenu.java
+++ Temp5-welcome-pizza/src/main/java/com/pizzamukza/pizzahut/domain/admin/pizzamenu/dto/PizzaMenu.java
@@ -1,21 +1,21 @@
 package com.pizzamukza.pizzahut.domain.admin.pizzamenu.dto;
 
 public class PizzaMenu {
-  int pizzaId;
+  int id;
   String pizzaName; // 페퍼로니피자
   String size; // 미디움, 라지
   int price;
 
   @Override
   public String toString() {
-    return pizzaName + " " + size + " " + price + "원 " + quantity + "개";
+    return id + " " + pizzaName + " " + size + " " + price + "원 " + quantity + "개";
   }
 
 
   int quantity; // 가격
 
   public PizzaMenu(int sizeId, String pizzaName, String sizeName, int price, int quantity) {
-    this.pizzaId = sizeId;
+    this.id = sizeId;
     this.pizzaName = pizzaName;
     this.size = sizeName;
     this.price = price;
