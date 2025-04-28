61578fe6d265691e383949ed1b77cf19e6604ff5
hyeblee <hyeb1224@naver.com>
2025-04-29 04:25:26 +0900

    feat: OrderController에서 추가 주문 없는 버전 추가

--- src/main/java/com/pizzamukza/pizzahut/domain/user/pizzamenu/controller/OrderController.java
+++ src/main/java/com/pizzamukza/pizzahut/domain/user/pizzamenu/controller/OrderController.java
@@ -1,64 +1,25 @@
 package com.pizzamukza.pizzahut.domain.user.pizzamenu.controller;
 
-import com.pizzamukza.pizzahut.domain.admin.sidemenu.dto.SideMenuDTO;
-import com.pizzamukza.pizzahut.domain.user.pizzamenu.dto.Pizza;
 import com.pizzamukza.pizzahut.domain.user.pizzamenu.service.OrderService;
 import com.pizzamukza.pizzahut.domain.user.pizzamenu.view.OrderView;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 public class OrderController {
-
-  private final OrderView orderView;
-  private final OrderService orderService;
-  public Scanner sc = new Scanner(System.in);
   public OrderController() {
-    this.orderView = new OrderView();
-    this.orderService = new OrderService();
-  }
+    OrderView orderView = new OrderView();
+    OrderService pizzaService = new OrderService();
 
-  public void startOrder() {
     int[] pizza;
     int[] side;
-
-    // 피자 주문
     orderView.displayPizzaOrderMenu();
-    List<Pizza> pizzaList = new ArrayList<>();
-
-    while (true) {
-      pizza = orderView.insertPizza();
-      for (int p : pizza) {
-        pizzaList.add(p);
-      }
-      System.out.print("🍕 피자를 추가로 주문하시겠습니까? (Y/N): ");
-      String answer = orderView.sc.nextLine().trim().toUpperCase();
-      if (!answer.equals("Y")) {
-        break;
-      }
-    }
-    orderService.decreasePizza(pizzaList.stream().mapToInt(i -> i).toArray());
-
-    // 사이드 주문
+    pizza = orderView.insertPizza();
+    OrderService.decreasePizza(pizza);
     orderView.displaySideMenu();
-    List<SideMenuDTO> sideList = new ArrayList<>();
-
-    while (true) {
-      side = orderView.insertSide();
-      for (int s : side) {
-        sideList.add(s);
-      }
-      System.out.print("🍟 사이드를 추가로 주문하시겠습니까? (Y/N): ");
-      String answer = sc.nextLine().trim().toUpperCase();
-      if (!answer.equals("Y")) {
-        break;
-      }
-    }
-    orderService.decreaseSide(sideList.stream().mapToInt(i -> i).toArray());
+    side = orderView.insertSide();
+    OrderService.decreaseSide(side);
   }
 
   public static void main(String[] args) {
     OrderController orderController = new OrderController();
-    orderController.startOrder();
+
   }
 }
